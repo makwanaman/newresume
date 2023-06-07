7a82eb29dd47587b6155d3ce6596268a80112c1c
aman <makwanaman1998@gmail.com>
2023-06-07 11:15:39 +0530

    node modules added

--- src/view/AllTemplate/ResumeSummary.jsx
+++ src/view/AllTemplate/ResumeSummary.jsx
@@ -32,7 +32,7 @@ const ResumeSummary = ({ preview }) => {
             ) : (
               <>
                 {t(
-                  "Motivated7 Sales Associate with 5 years of experience boosting sales and customer loyalty through individualized service. Resourceful expert at learning customer needs, directing to desirable merchandise and upselling to meet sales quotas. Committed to strengthening customer experiences with positivity and professionalism when answering requests and processing sales."
+                  "Motivated Sales Associate with 5 years of experience boosting sales and customer loyalty through individualized service. Resourceful expert at learning customer needs, directing to desirable merchandise and upselling to meet sales quotas. Committed to strengthening customer experiences with positivity and professionalism when answering requests and processing sales."
                 )}
               </>
             )}
